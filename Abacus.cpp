53e30efe583869a0851f036e559ffd98b331ae87
Cavan Theiss <cavanrt@gmail.com>
2025-08-16 18:59:14 -0500

    Update Abacus.cpp

--- Abacus.cpp
+++ Abacus.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
 using namespace std; 
+*/
 
+I think there will be at least 3 vector arrays, possibly 4 depending on how I want to handle "Carry over"
+
+  Carry over    :[ ]
+  Top Vector    :[ , ,]
+  Bottom Vector :[ , ,]
+  Product       :[ , ,]
+
+  Each vector contains the individual digits of each number seperately so as to calculate much larger numbers
+  
+  each step multiplies top and bottom to get product, the product moves to top and the bottom goes down by one
+  
+/*
 int main {
 
 return 0;
