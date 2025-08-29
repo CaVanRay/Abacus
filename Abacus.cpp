b681cad969fb41e21785e3f74bdee03e0cdf1d2b
Cavan Theiss <cavanrt@gmail.com>
2025-08-29 10:56:43 -0500

    Update Abacus.cpp
    Ok, vector array was going in the wrong direction but I fixed it.
    
    also, the outer for loop is temporarily shut off for testing, I don't need to see 536 iterations on the screen.

--- Abacus.cpp
+++ Abacus.cpp
@@ -5,23 +5,26 @@
 using namespace std;
 
 int main() {
+    int factInput = 536;
+    string toConvert;
+    vector<int> TopV, BotV, ProdV;
 
-int factInput;
-string toConvert;
-vector<int> TopV, BotV, ProdV;
+    // Convert number into digits
+    toConvert = to_string(factInput);
+    for (char c : toConvert) { 
+        TopV.insert(TopV.begin(),(c - '0'));
+    }
+    BotV = TopV;
 
-cout << "Please provide a number [1 < N < 100] : " ;
-cin >> factInput;
-toConvert = to_string(factInput);
+    // Loop through indices instead of values
+ //   for (int k = factInput; k > 0; k--) {
+        for (size_t i = 0; i < TopV.size(); i++) {
+            for (size_t j = 0; j < BotV.size(); j++) {
+                cout << "TopV[" << i << "] = " << TopV[i] << endl;
+                cout << "BotV[" << j << "] = " << BotV[j] << endl << endl;
+            }
+        }
+  //  }
 
-for (char c : toConvert) { 
-    TopV.push_back(c - '0');
-}
-
-for (int items : TopV) {
-    cout << items << " ";
-}
-
-
-return 0;
+    return 0;
 }
