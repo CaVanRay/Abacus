0eee2a34ac0fe91568774f13a06b6fba0b7ba0f3
Cavan Theiss <cavanrt@gmail.com>
2025-08-29 12:56:59 -0500

    Update Abacus.cpp

--- Abacus.cpp
+++ Abacus.cpp
@@ -6,30 +6,34 @@ using namespace std;
 
 int main() {
     
-    int factInput = 536;
+    int factInput = 25;
     string toConvert;
     vector<int> TopV, BotV, ProdV;
 
-    // Convert number into digits
+    // String/Digit Converter
     toConvert = to_string(factInput);
     for (char c : toConvert) { 
         TopV.insert(TopV.begin(),(c - '0'));
     }
     
-    BotV = TopV;
     int tempMult, carry = 0;
 
+    for (int y = factInput; y >= 1; y--){
+
+        BotV.clear();
+        toConvert = to_string(y);
+        for (char c : toConvert) {
+            BotV.insert(BotV.begin(), (c - '0'));
+        }
+
+        ProdV.clear();
+        ProdV.resize(TopV.size() + BotV.size() + 1);
+        
         for (size_t i = 0; i < TopV.size(); i++) {
             int proLocation = i;
             
-            for (size_t j = 0; j < BotV.size(); j++) {
-                
-                cout << "TopV[" << i << "] = " << TopV[i] << endl;
-                cout << "BotV[" << j << "] = " << BotV[j] << endl << endl;
-                
-                ProdV.resize( TopV.size() + BotV.size() + 1);
-                
-                tempMult = TopV[i] * BotV[j];
+            for (size_t j = 0; j < BotV.size(); j++) {               
+                tempMult = (TopV[i] * BotV[j]);
                 
                 while(tempMult > 9){
                     
@@ -37,17 +41,26 @@ int main() {
                     carry = carry + 1;
                 }
                 
-                ProdV[proLocation] = ProdV[proLocation] + (tempMult);
-                ProdV[proLocation + 1] = ProdV[proLocation + 1] + carry;
-                carry = 0;
+            ProdV[proLocation] += tempMult;
+            ProdV[proLocation + 1] += carry;
+            carry = 0;
                 
             proLocation++;
             }
         }
+    
+        for (int x = ProdV.size(); x >= 0; x--){
+            if(ProdV[x] == 0){
+                ProdV.erase(ProdV.begin() + x);
+            }else break;
+        }
+    TopV = ProdV; 
+    ProdV.clear();
 
+    }
     cout << "Product = ";
-    for (size_t k = (ProdV.size()); k > 0; k--){
-        cout << ProdV[k];
+    for (int k = (TopV.size()); k >= 0; k--){
+        cout << TopV[k];
     } 
     return 0;
 }
