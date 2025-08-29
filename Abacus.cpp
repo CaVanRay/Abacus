38e367f53e814343782d7bc4047fe597d0860bf0
Cavan Theiss <cavanrt@gmail.com>
2025-08-29 09:58:10 -0500

    Update Abacus.cpp

--- Abacus.cpp
+++ Abacus.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
+
+int FactInput;
+vector<int> TopV[]
+
+cout >> "Please provide a number [1 < N < 100] : " ;
+cin << FactInput;
+
+
+
 
 return 0;
 }
