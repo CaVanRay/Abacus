9677a36242f50731808955b1248d345942238671
Cavan Theiss <cavanrt@gmail.com>
2025-08-29 10:06:49 -0500

    Update Abacus.cpp
    First try, everything works.
    
    this is just taking input and splitting the digits into a vector array as separate items
    
    next is expanding and adding multiplication

--- Abacus.cpp
+++ Abacus.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
 using namespace std;
 
 int main() {
 
-int FactInput;
-vector<int> TopV[]
+int factInput;
+string toConvert;
+vector<int> TopV, BotV, ProdV;
 
-cout >> "Please provide a number [1 < N < 100] : " ;
-cin << FactInput;
+cout << "Please provide a number [1 < N < 100] : " ;
+cin >> factInput;
+toConvert = to_string(factInput);
 
+for (char c : toConvert) { 
+    TopV.push_back(c - '0');
+}
 
+for (int items : TopV) {
+    cout << items << " ";
+}
 
 
 return 0;
