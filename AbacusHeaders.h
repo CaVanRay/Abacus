31646ba2e18d039dfc9ee267cce3fbe4918c901a
Cavan Theiss <cavanrt@gmail.com>
2026-05-13 13:37:54 -0500

    Update AbacusHeaders.h

--- AbacusHeaders.h
+++ AbacusHeaders.h
@@ -1,10 +1,13 @@
+#ifndef ABACUSHEADERS_H
+#define ABACUSHEADERS_H
 // official libraries
 #include <iostream>
 #include <string>
-// my own headers
+// my own headers elf
 #include "AbacusMenu.h"
 #include "AbacusMultiply"
 #include "AbacusDivide"
 #include "AbacusAddition"
 #include "AbacusSubtraction"
 
+#endif
