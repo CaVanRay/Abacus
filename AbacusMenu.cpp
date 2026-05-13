37c01a20acb93d7409c76db0bd463ff8c4ddfe15
Cavan Theiss <cavanrt@gmail.com>
2026-05-13 15:12:38 -0500

    Update AbacusMenu.cpp

--- AbacusMenu.cpp
+++ AbacusMenu.cpp
@@ -1,1 +1,11 @@
+#include "AbacusHeaders.h"
 
+int main(){
+  std::cout << "********** Main Menu **********" << std::endl:
+  std::cout << " 1) - Addition" << std::endl;
+  std::cout << " 2) - Subtraction" << std::endl;
+  std::cout << " 3) - Multiplication" << std::endl;
+  std::cout << " 4) - Division" << std::endl;
+  std::cout << "********************************" << std::endl;
+  std::cout << "      select action (1-4) : ";
+}
