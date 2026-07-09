61dac758f4b6d8b5b71f9f95cf4b2d7c64ec8ac2
Cavan Theiss <cavanrt@gmail.com>
2026-07-09 16:34:35 -0500

    Update Divide.cpp

--- SourceFiles/Divide.cpp
+++ SourceFiles/Divide.cpp
@@ -52,9 +52,9 @@ int main(){
     //     dividendString.erase(0, 1);
     // }
 
-    while(divisorString[0] == '0' && divisorString.size() > 1){
-        divisorString.erase(0, 1);
-    }
+    // while(divisorString[0] == '0' && divisorString.size() > 1){
+    //     divisorString.erase(0, 1);
+    // }
 
     // CONVERT LARGE NUMBER STRINGS INTO VECTORS
 
